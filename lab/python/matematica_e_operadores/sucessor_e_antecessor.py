014991969282209507c11e0f76247c7ae3fe62b5
Jv131103 <jjv1792@gmail.com>
2025-09-04 08:57:32 -0300

    update/atualizacao 04/09/2025

---
+++ lab/python/matematica_e_operadores/sucessor_e_antecessor.py
@@ -0,0 +1,10 @@
+def sucessor_e_antecessor(numero):
+    sucessor = numero + 1
+    antecessor = numero - 1
+
+    print(f"Antecessor: {antecessor}")
+    print(f"Sucessor: {sucessor}")
+
+
+numero = int(input())
+sucessor_e_antecessor(numero)
---
+++ lab/python/temperaturas/celcius_to_farenheit.py
@@ -0,0 +1,6 @@
+def celcius_to_farenheit(celcius):
+    return celcius * 9/5 + 32
+
+
+c = int(input())
+print(celcius_to_farenheit(c))
